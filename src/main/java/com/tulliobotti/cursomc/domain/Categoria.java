fbe5ce7bd7b775f02b8b99199129e5af7177773c
Tullio Botti <tulliobotti@gmail.com>
2019-12-16 09:52:04 +0100

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/tulliobotti/cursomc/domain/Categoria.java
+++ src/main/java/com/tulliobotti/cursomc/domain/Categoria.java
@@ -2,9 +2,19 @@ package com.tulliobotti.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+//para indicar ao JPA que essa classe tera persistencia
+@Entity
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	//para definir como vai ser criado o id dentro da tabela - o IDENTITY funciona com o H2
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	public Categoria() {
