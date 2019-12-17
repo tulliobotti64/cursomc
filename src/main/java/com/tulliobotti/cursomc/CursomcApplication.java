1de80a55454c749cd2dba85f62b419dfca3075fe
Tullio Botti <tulliobotti@gmail.com>
2019-12-17 12:09:53 +0100

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/tulliobotti/cursomc/CursomcApplication.java
+++ src/main/java/com/tulliobotti/cursomc/CursomcApplication.java
@@ -1,24 +1,27 @@
 package com.tulliobotti.cursomc;
 
 import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import com.tulliobotti.cursomc.domain.Categoria;
 import com.tulliobotti.cursomc.domain.Cidade;
+import com.tulliobotti.cursomc.domain.Cliente;
+import com.tulliobotti.cursomc.domain.Endereco;
 import com.tulliobotti.cursomc.domain.Estado;
 import com.tulliobotti.cursomc.domain.Produto;
+import com.tulliobotti.cursomc.domain.enums.TipoCliente;
 import com.tulliobotti.cursomc.repositories.CategoriaRepository;
 import com.tulliobotti.cursomc.repositories.CidadeRepository;
+import com.tulliobotti.cursomc.repositories.ClienteRepository;
+import com.tulliobotti.cursomc.repositories.EnderecoRepository;
 import com.tulliobotti.cursomc.repositories.EstadoRepository;
 import com.tulliobotti.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
-
+	//declaracao das dependencias
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	@Autowired
@@ -27,6 +30,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private CidadeRepository cidadeRepository;
 	@Autowired
 	private EstadoRepository estadoRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -66,6 +73,25 @@ public class CursomcApplication implements CommandLineRunner {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "mariasilva@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefone().addAll(Arrays.asList("27363323","93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "apto 203", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);	
+		//associar os enderecos ao cliente
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
 		
 	}
 }
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/tulliobotti/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.tulliobotti.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tulliobotti.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}
